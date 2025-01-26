30a100e448473b84d55404e9c7e53292b251e211
manik-besnik <manikmia.dev@gmail.com>
2025-01-27 01:09:39 +0600

    update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->unsignedTinyInteger('role_id')->default(2)->nullable();
             $table->string('provider_id')->nullable();
             $table->string('name')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->string('username')->unique();
             $table->string('avatar')->nullable();
