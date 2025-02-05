9c79176ce16b243e3ad123f02dc2eb55abd7bff6
manik-besnik <manik.besnik@gmail.com>
2025-02-05 09:23:26 +0600

    update

---
+++ app/Actions/Admin/JobPost/JobApplicationAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Actions\Admin\JobPost;
+
+use App\Models\JobApply;
+use Inertia\Inertia;
+
+class JobApplicationAction
+{
+    public function execute(): \Inertia\Response
+    {
+        $applications  = JobApply::query()->paginate(20);
+
+        return Inertia::render('Admin/JobPost/Apply',[
+            'job_applies' => $applications
+        ]);
+    }
+}
