7a282ce21e7931cc58546c160f1a28e6c4b30bb3
manik-besnik <manik.besnik@gmail.com>
2025-02-11 16:48:28 +0600

    update

--- resources/js/ssr.jsx
+++ resources/js/ssr.jsx
@@ -4,7 +4,7 @@ import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import route from 'ziggy-js';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Dubai E-Visa';
 
 createServer((page) =>
     createInertiaApp({
