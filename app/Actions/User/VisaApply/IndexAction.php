a047510ce4438220cd952c3df2a9b85f55755408
manik-besnik <manik.besnik@gmail.com>
2025-02-11 12:03:43 +0600

    Search Update

--- app/Actions/User/VisaApply/IndexAction.php
+++ app/Actions/User/VisaApply/IndexAction.php
@@ -13,6 +13,7 @@ class IndexAction
 
         $name = request()->input('name');
         $appId = request()->input('app_id');
+        $currentNationality = request()->input('current_nationality');
         $processingType = request()->input('processing_type');
         $visaType = request()->input('visa_type');
         $fromDate = request()->input('from_date');
@@ -33,6 +34,7 @@ class IndexAction
             ->when($group, fn($query) => $query->where('group', $group))
             ->when($visaStatus, fn($query) => $query->where('status', $visaStatus))
             ->when($personalName, fn($query) => $query->whereHas('personalInfo', fn($q) => $q->where('name', $personalName)))
+            ->when($currentNationality, fn($query) => $query->whereHas('personalInfo', fn($q) => $q->where('current_nationality', $currentNationality)))
             ->when($passportNo, fn($query) => $query->whereHas('passport', fn($q) => $q->where('passport_no', $passportNo)))
             ->get();
 
