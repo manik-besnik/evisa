1a3bc5c686b18c90520bb720d2e383f3f081e023
manik-besnik <manik.besnik@gmail.com>
2025-02-14 07:53:52 +0600

    Check Permissions

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -3,23 +3,25 @@ import Dropdown from '@/Components/Dropdown';
 import BarIcon from '@/Components/SvgIcons/Bar';
 import Avatar from "@/Components/SvgIcons/Avatar.jsx";
 import Notification from "@/Components/Admin/Notification.jsx";
+import {isPermitted} from "@/Components/Helper/index.js";
+import {permissionEnums} from "@/Components/Constant/index.js";
 
 export default function NavBar({useToggleSideNav}) {
 
-
     const user = usePage().props.auth.user;
-
 
     return (
         <div className='flex justify-between py-1.5 px-4 lg:py-4 bg-white lg:rounded-[8px]'>
 
             <div className='hidden lg:flex items-center gap-[10px]'>
-                <Link href={route('admin.admins.index')}
-                      className={`${route().current() === 'admin.admins.index' && 'bg-card-and-hover'} px-3 py-2 text-[14px] font-medium leading-[20px] text-text-primary hover:bg-side-and-button rounded-md`}> Admins
-                </Link>
-                <Link href={route('admin.roles.index')}
-                      className={`${route().current() === 'admin.roles.index' && 'bg-card-and-hover'} px-3 py-2 text-[14px] font-medium leading-[20px] text-text-primary hover:bg-side-and-button rounded-md`}> Roles
-                </Link>
+                {isPermitted(permissionEnums.VIEW_ADMIN) &&
+                    <Link href={route('admin.admins.index')}
+                          className={`${route().current() === 'admin.admins.index' && 'bg-card-and-hover'} px-3 py-2 text-[14px] font-medium leading-[20px] text-text-primary hover:bg-side-and-button rounded-md`}> Admins
+                    </Link>}
+                {isPermitted(permissionEnums.VIEW_ROLE) &&
+                    <Link href={route('admin.roles.index')}
+                          className={`${route().current() === 'admin.roles.index' && 'bg-card-and-hover'} px-3 py-2 text-[14px] font-medium leading-[20px] text-text-primary hover:bg-side-and-button rounded-md`}> Roles
+                    </Link>}
             </div>
 
             <button className='lg:hidden' onClick={() => useToggleSideNav(true)}>
