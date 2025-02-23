092c9ddf8bc6f4ea48fcd6bbe1358b9cde719509
manik-besnik <manik.besnik@gmail.com>
2025-02-23 07:25:28 +0600

    update

--- resources/js/Components/Web/ServiceImage.jsx
+++ resources/js/Components/Web/ServiceImage.jsx
@@ -1,7 +1,7 @@
 
 const ServiceImage = () => {
     return (
-        <img className="w-[180px] h-auto" src='https://dubaievisaservice.com/publicassets/images/services.png' alt="welcome to dubai e-visa"/>
+        <img className="w-[180px] h-auto" src='https://dubaievisaservice.com/public/assets/images/services.png' alt="welcome to dubai e-visa"/>
     )
 }
 export default ServiceImage
