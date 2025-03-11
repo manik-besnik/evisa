f48932727c7bd9c4523aebdd9ae092abc6c18b6b
Manik Mia <135939845+manik-besnik@users.noreply.github.com>
2025-03-11 19:43:50 +0600

    Add Relation to company Model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  *
@@ -15,10 +17,18 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $city
  * @property string $area
  * @property string $address
+ *
+ * @mixin Model
+ * @property JobDemand<int,Collection> $jobDemands
  */
 class Company extends Model
 {
     protected $table = 'companies';
 
     protected $fillable = ['name', 'contact_name', 'email', 'phone_no', 'whatsapp_no', 'city', 'area', 'address'];
+
+    public function jobDemands(): HasMany
+    {
+        return $this->hasMany(JobDemand::class, 'company_id', 'id');
+    }
 }
