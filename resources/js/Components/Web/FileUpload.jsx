ee26c90f20f06122e7dcdc420ef0afd65c6fcc2d
Manik Mia <135939845+manik-besnik@users.noreply.github.com>
2025-03-15 22:29:21 +0600

    update

---
+++ resources/js/Components/Web/FileUpload.jsx
@@ -0,0 +1,75 @@
+import {useState} from "react";
+import {GrAttachment} from "react-icons/gr";
+
+const InputFile = ({
+                       defaultClasses = "w-full h-auto",
+                       classes = '',
+                       placeholder = '',
+                       onChange,
+                       fileType = '',
+                       error = "",
+                       children = null
+                   }) => {
+
+    const [fileName, setFileName] = useState(placeholder);
+    const [readFile, setReadFile] = useState(null)
+
+    const handleUploadFile = (e) => {
+        const file = e.target.files[0]
+
+        if (file) {
+            const validTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/svg', 'image/webp', 'image/jpg', 'application/pdf'];
+            if (!validTypes.includes(file.type)) {
+
+                setFileName(placeholder);
+                return;
+            }
+
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setReadFile(reader.result);
+            };
+
+            reader.readAsDataURL(file);
+
+            onChange(fileType, file)
+
+            setFileName("Attached");
+        } else {
+            setFileName(placeholder);
+        }
+    }
+
+    return (
+        <div
+            className={`${defaultClasses} ${classes} ${fileName !== placeholder ? "bg-[#aec0d5]" : 'bg-[#E0EBF8]'}`}>
+            <label className="w-full h-full cursor-pointer"
+                   htmlFor={`file-upload-${fileType}`}>
+                <input
+                    onChange={(e) => handleUploadFile(e)}
+                    id={`file-upload-${fileType}`}
+                    type="file"
+                    accept=".pdf,.png,.jpg,.jpeg,.webp"
+                    className="hidden"
+                />
+
+                {readFile ? (
+                    <img src={readFile} className="w-full h-auto" alt="Uploaded file"/>
+                ) : (
+                    children ? children : (
+                        <div className="flex flex-col items-center">
+                            <GrAttachment size={15}/>
+                            <p className="text-xs break-words">{fileName ? fileName : placeholder}</p>
+                        </div>
+                    )
+                )}
+                {error && <p className="text-xs text-warning">{error}</p>}
+
+
+            </label>
+        </div>
+    )
+}
+
+
+export default InputFile
---
+++ resources/js/Components/Web/JobDemandBanner.jsx
@@ -0,0 +1,24 @@
+import {FaCameraRetro} from "react-icons/fa";
+
+const JobDemandBanner = () => {
+    return (<>
+        <div className="w-full bg-gray-500 text-white p-4 flex justify-between items-center">
+            <div className="flex items-center space-x-4">
+                <div className="flex">
+                    <div className="h-6 w-3 bg-yellow-500 mr-0.5"></div>
+                    <div className="h-6 w-3 bg-yellow-500 mr-0.5"></div>
+                    <div className="h-6 w-3 bg-yellow-500"></div>
+                </div>
+            </div>
+        </div>
+
+        {/* Camera Icon Space */}
+        <div className="relative h-32 bg-gray-500">
+            <div className="absolute bottom-4 right-4 text-white">
+                <FaCameraRetro size={30} className="text-white"/>
+            </div>
+        </div>
+    </>)
+}
+
+export default JobDemandBanner
