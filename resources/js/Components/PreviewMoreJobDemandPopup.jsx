9794554d9e1d21b84fce4bfcd41b2ea32d34016e
Rafiqul-islam-dev <rafiqulislam93.bd@gmail.com>
2025-03-17 21:29:32 +0600

    update

--- resources/js/Components/PreviewMoreJobDemandPopup.jsx
+++ resources/js/Components/PreviewMoreJobDemandPopup.jsx
@@ -3,6 +3,8 @@ import { FaWhatsapp, FaDownload, FaShare, FaFacebook } from "react-icons/fa";
 import { assetUrl } from "@/Components/Constant/index.js";
 
 const PreviewMoreJobDemandPopup = ({ isOpen, onClose, data }) => {
+    console.log(data);
+    
     if (!isOpen) return null;
 
     return (
@@ -17,19 +19,28 @@ const PreviewMoreJobDemandPopup = ({ isOpen, onClose, data }) => {
                     </svg>
                 </button>
 
-                {/* Header with Security and Camera Icon */}
-                <div className="bg-gray-500 text-white p-4 flex justify-between items-center">
-                    <div className="text-2xl font-bold">{data.type_of_work}</div>
-                </div>
+                
 
                 {/* Camera Space */}
-                <div className="h-32 bg-gray-500 relative">
-                    <div className="absolute bottom-4 right-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                        </svg>
-                    </div>
+                {/* Camera Space */}
+                <div className="h-32 bg-gray-200 relative mt-5">
+                    {data.thumbnailPreview ? (
+                        <div className="w-full h-full flex items-center justify-center">
+                            <img
+                                src={data.thumbnailPreview}
+                                alt="Job Thumbnail"
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                    ) : (
+                        <div className="w-full h-full flex items-center justify-center bg-gray-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+                            </svg>
+                            <span className="ml-2 text-gray-600">No image available</span>
+                        </div>
+                    )}
                 </div>
 
 
