09423e4b9f99dac2140165745a05569bec7efc43
Manik Mia <135939845+manik-besnik@users.noreply.github.com>
2025-03-19 23:41:26 +0600

    Css Issue fixed

--- resources/js/Pages/Job.jsx
+++ resources/js/Pages/Job.jsx
@@ -1,5 +1,5 @@
 import WebLayout from "@/Layouts/WebLayout.jsx";
-import { Head, Link } from "@inertiajs/react";
+import {Head, Link} from "@inertiajs/react";
 import SearchContainer from "@/Components/Web/SearchContainer.jsx";
 import {
     assetUrl,
@@ -8,60 +8,66 @@ import {
 const Job = () => {
     return (
         <WebLayout showBgImage={true} showServiceImage={true}>
-            <Head title="Other | Dubai E-Visa" />
+            <Head title="Other | Dubai E-Visa"/>
             <div className="container">
                 <div className="grid grid-cols-2 gap-x-20">
-                    <SearchContainer />
+                    <SearchContainer/>
                     <div className="w-1/2 h-[80vh]">
                         <div className="bg-[#6b7377c8] h-full relative p-5">
                             {/* Job Directory Card 1 */}
                             <Link href={route('job.directory')} className="block mb-8">
-                            <div className="flex flex-col items-center mb-8">
-                                    <div className="bg-white w-36 h-36 relative rounded-[6px] border-2 border-[#5A5B5A]">
-                                    <img
-                                        src={`${assetUrl + 'images/jobdirectory.png'}`}
-                                        alt="Job Directory"
-                                        className="m-auto mt-2"
-                                    />
+                                <div className="flex flex-col items-center mb-8">
+                                    <div
+                                        className="bg-white w-36 h-40 relative rounded-[6px] border-2 border-[#5A5B5A]">
+                                        <img
+                                            src={`${assetUrl + 'images/jobdirectory.png'}`}
+                                            alt="Job Directory"
+                                            className="m-auto my-2"
+                                        />
 
-                                        <div className="bg-red-600 p-2 text-white w-full text-center absolute bottom-0 rounded-bl-[4px] rounded-br-[4px]">
-                                            <span className="font-bold text-[18px]">Job Directory</span>
+                                        <div
+                                            className="bg-red-600 mt-1 p-2 text-white w-full text-center absolute bottom-0 rounded-bl-[4px] rounded-br-[4px]">
+                                            <span className="font-bold text-[16px]">Job Directory</span>
+                                        </div>
                                     </div>
+
                                 </div>
-                                
-                            </div>
-                          </Link>
+                            </Link>
                             {/* Job Directory Card 2 */}
                             <Link href={route('job-demand.create')} className="block mb-8">
-                            <div className="flex flex-col items-center mb-8">
-                                    <div className="bg-white w-36 h-36 relative rounded-[6px] border-2 border-[#5A5B5A]">
-                                    <img
-                                        src={`${assetUrl + 'images/jodemand.png'}`}
-                                        alt="Job Directory"
-                                        className="m-auto mt-2"
-                                    />
-                                        <div className="bg-red-600 p-2 text-white w-full text-center  absolute bottom-0 rounded-bl-[4px] rounded-br-[4px]">
-                                            <span className="font-bold text-[18px]">Job Demand</span>
+                                <div className="flex flex-col items-center mb-8">
+                                    <div
+                                        className="bg-white w-36 h-36 relative rounded-[6px] border-2 border-[#5A5B5A]">
+                                        <img
+                                            src={`${assetUrl + 'images/jodemand.png'}`}
+                                            alt="Job Directory"
+                                            className="m-auto mt-2"
+                                        />
+                                        <div
+                                            className="bg-red-600 p-2 text-white w-full text-center  absolute bottom-0 rounded-bl-[4px] rounded-br-[4px]">
+                                            <span className="font-bold text-[16px]">Job Demand</span>
                                         </div>
+                                    </div>
+
                                 </div>
-                                
-                            </div>
-                        </Link>
+                            </Link>
                             {/* General Apply Card */}
                             <Link href={route('job-posts.create')} className="block mb-8">
-                            <div className="flex flex-col items-center">
-                                    <div className="bg-white w-36 h-32 relative rounded-[6px] border-2 border-[#5A5B5A]">
-                                    <img
-                                        src={`${assetUrl + 'images/generalapply.png'}`}
-                                        alt="General Apply"
-                                        className="m-auto mt-4"
-                                    />
-                                        <div className="bg-green-600 p-2 text-white w-full text-center absolute bottom-0 rounded-bl-[4px] rounded-br-[4px]">
-                                            <span className="font-bold text-[18px]">General Apply</span>
+                                <div className="flex flex-col items-center">
+                                    <div
+                                        className="bg-white w-36 h-32 relative rounded-[6px] border-2 border-[#5A5B5A]">
+                                        <img
+                                            src={`${assetUrl + 'images/generalapply.png'}`}
+                                            alt="General Apply"
+                                            className="m-auto mt-4"
+                                        />
+                                        <div
+                                            className="bg-green-600 p-2 text-white w-full text-center absolute bottom-0 rounded-bl-[4px] rounded-br-[4px]">
+                                            <span className="font-bold text-[16px]">General Apply</span>
                                         </div>
+                                    </div>
+
                                 </div>
-                                
-                            </div>
                             </Link>
                         </div>
                     </div>
