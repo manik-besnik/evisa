f1cf6d8f75136d5cce1dbd775ab0a06c2afaa063
Manik Mia <135939845+manik-besnik@users.noreply.github.com>
2025-04-20 22:19:25 +0600

    update

--- app/Actions/User/CvCreate/CreateAction.php
+++ app/Actions/User/CvCreate/CreateAction.php
@@ -5,23 +5,21 @@ namespace App\Actions\User\CvCreate;
 use App\Models\Language;
 use App\Models\Location;
 use App\Models\UserCV;
-use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class CreateAction
 {
-    public function execute(): \Inertia\Response|RedirectResponse
+    public function execute(): Response
     {
 
-        $cv = UserCv::query()->where('user_id', auth()->id())->exists();
+        $cv = UserCv::query()->where('user_id', auth()->id())->first();
 
-        if ($cv) {
-            return to_route('cv.index');
-        }
 
         return Inertia::render('UserCV/CvCreate', [
             'locations' => Location::query()->select(['id', 'name'])->get(),
             'languages' => Language::query()->select(['id', 'name'])->get(),
+            'cv' => $cv
         ]);
     }
 }
