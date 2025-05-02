e265bafac1ea9dc1bea4b6d63230e6b593d7189f
Rafiqul-islam-dev <rafiqulislam93.bd@gmail.com>
2025-05-02 11:40:26 +0600

    update

--- resources/views/pdfs/test.blade.php
+++ resources/views/pdfs/test.blade.php
@@ -326,13 +326,13 @@
                 </div>
                 <div class="contact-item">
                     <div class="contact-icon">
-                        <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/message.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 20px; height: 20px; margin-top: 4px; border-radius: 0">
+                        <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/message.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 20px; height: 15px; margin-top: 4px; border-radius: 0">
                     </div>
                     <div class="contact-text">{{$cv->email}}</div>
                 </div>
                 <div class="contact-item">
                     <div class="contact-icon">
-                        <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/link.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 20px; height: 20px; margin-top: 4px;">
+                        <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/link.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 20px; height: 18px; margin-top: 4px; border-radius: 0">
                     </div>
                     <div class="contact-text">{{$cv->website}}</div>
                 </div>
@@ -342,9 +342,7 @@
         <div class="section">
             <div class="section-header">
                 <div class="icon-circle">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z" fill="white"/>
-                    </svg>
+                    <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/tik.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 24px; height: 24px; margin-top: 6px;">
                 </div>
                 <div class="header-text">Personal Skills</div>
             </div>
@@ -362,11 +360,7 @@
         <div class="section">
             <div class="section-header">
                 <div class="icon-circle">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 21.35C15.0787 21.35 17.75 18.6787 17.75 15.6C17.75 12.5213 15.0787 9.85 12 9.85C8.92129 9.85 6.25 12.5213 6.25 15.6C6.25 18.6787 8.92129 21.35 12 21.35Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M12 6.84998C14.2091 6.84998 16 8.64098 16 10.85C16 13.059 14.2091 14.85 12 14.85C9.79086 14.85 8 13.059 8 10.85C8 8.64098 9.79086 6.84998 12 6.84998Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M4.92999 4.92996C6.9849 2.87495 9.8509 2 12.9299 2C17.3929 2 20.9299 4.53704 21.7599 8.00004C21.9999 8.74999 21.9999 9.60004 21.7599 10.25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/love.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 24px; height: 24px; margin-top: 6px;">
                 </div>
                 <div class="header-text">Interests</div>
             </div>
@@ -387,10 +381,7 @@
         <div class="section">
             <div class="section-header">
                 <div class="icon-circle">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 14C14.7614 14 17 11.7614 17 9C17 6.23858 14.7614 4 12 4C9.23858 4 7 6.23858 7 9C7 11.7614 9.23858 14 12 14Z" stroke="#1a222e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M20.5899 22.0001C20.5899 18.6867 17.8987 15.9998 14.5899 15.9998C13.583 15.9998 12.6604 16.171 11.8489 16.4865C11.0374 16.171 10.1148 15.9998 9.1079 15.9998C5.80002 15.9998 3.09993 18.6867 3.09993 22.0001" stroke="#1a222e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/user.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 20px; height: 20px; margin-top: 6px; border-radius: 0">
                 </div>
                 <div class="header-text">About Me</div>
             </div>
@@ -403,10 +394,7 @@
         <div class="section">
             <div class="section-header">
                 <div class="icon-circle">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21 16V8C21 4 18 2 14 2H10C6 2 4 4 4 8V16C4 20 6 22 10 22H14C18 22 21 20 21 16Z" stroke="#1a222e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M16 2V6H8V2" stroke="#1a222e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                   <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/officebag.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 20px; height: 20px; margin-top: 6px; border-radius: 0">
                 </div>
                 <div class="header-text">Experience</div>
             </div>
@@ -431,10 +419,7 @@
         <div class="section">
             <div class="section-header">
                 <div class="icon-circle">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 2C15.1989 2 18.103 3.96514 19.6613 6.68321C20.3754 7.7057 20.625 8.99988 20.625 10.3125C20.625 15.5188 16.625 19.5188 12 19.5188C7.37502 19.5188 3.37502 15.5188 3.37502 10.3125C3.37502 8.99988 3.62461 7.7057 4.33874 6.68321C5.89695 3.96514 8.80107 2 12 2Z" stroke="#1a222e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M8.5 8.5L10.5 12.5L15.5 10.5" stroke="#1a222e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                   <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/book.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 20px; height: 15px; margin-top: 12px; border-radius: 0">
                 </div>
                 <div class="header-text">Education</div>
             </div>
@@ -458,10 +443,7 @@
         <div class="section">
             <div class="section-header">
                 <div class="icon-circle">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M3 4.99988H21" stroke="#1a222e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M7 9.99988C7 8.99988 7.9 8.09988 9 8.09988H15C16.1 8.09988 17 8.99988 17 9.99988V18.9999C17 19.9999 16.1 20.9999 15 20.9999H9C7.9 20.9999 7 19.9999 7 18.9999V9.99988Z" stroke="#1a222e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/flug.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 20px; height: 15px; margin-top: 11px; border-radius: 0">
                 </div>
                 <div class="header-text">Languages</div>
             </div>
@@ -476,9 +458,7 @@
         <div class="section">
             <div class="section-header">
                 <div class="icon-circle">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 12C14.2091 12 16 10.2091 16 8C16 5.79086 14.2091 4 12 4C9.79086 4 8 5.79086 8 8C8 10.2091 9.79086 12 12 12ZM12 14C10.3431 14 9 15.3431 9 17C9 18.6569 10.3431 20 12 20C13.6569 20 15 18.6569 15 17C15 15.3431 13.6569 14 12 14Z" fill="#1a222e"/>
-                    </svg>
+                    <img src="{{ public_path(str_replace(url('/'), '', 'assets/images/cv/useryellow.png')) }}" alt="{{$cv->name}}" class="profile-image" style="width: 20px; height: 20px; margin-top: 6px; border-radius: 0">
                 </div>
                 <div class="header-text">Reference</div>
             </div>
