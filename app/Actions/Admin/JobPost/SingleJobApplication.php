a9a066a73d03bc252e81b726f0edf362fececc88
Manik Mia <135939845+manik-besnik@users.noreply.github.com>
2025-05-02 12:19:41 +0600

    update

--- app/Actions/Admin/JobPost/SingleJobApplication.php
+++ app/Actions/Admin/JobPost/SingleJobApplication.php
@@ -11,7 +11,7 @@ class SingleJobApplication
     public function execute(int $id): \Inertia\Response
     {
         $jobApply = JobApply::query()
-            ->with(['education.language','experiences.country:id,name','country:id,nationality'])
+            ->with(['education.language','experiences.country:id,name','country:id,nationality','jobDemand:id,type_of_work'])
             ->findOrFail($id);
 
         return Inertia::render('Admin/JobPost/JobApplicationShow', [
